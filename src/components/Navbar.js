6ce85b5dce5463a04cc53656c402dad49b5f57db
bundy huang <bundyhuang1999@gmail.com>
2022-04-16 20:42:23 +1000

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const navClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
+
   return (
-    <div>Navbar</div>
-  )
+    <>
+    <nav className="Navbar">
+      <div className="navbar-con">
+        <Link to='/' className="navbar-logo">
+          TRVL <i className="fa-solid fa-cog fa-spin"></i>
+        </Link>
+        <div className="menu-icon" onClick = {navClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
+        <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMenu}>
+                Home
+              </Link>
+          </li>
+          <li className='nav-item'>
+              <Link to='/about' className='nav-links' onClick={closeMenu}>
+                About
+              </Link>
+          </li>
+          <li className='nav-item'>
+              <Link to='/work' className='nav-links' onClick={closeMenu}>
+                Work
+              </Link>
+          </li>
+          <li className='nav-item'>
+              <Link to='/contact' className='nav-links' onClick={closeMenu}>
+                Contact
+              </Link>
+          </li>
+          <li className='nav-item'>
+              <Link to='/blog' className='nav-links' onClick={closeMenu}>
+                Blog
+              </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </>
+  );
 }
 
 export default Navbar
